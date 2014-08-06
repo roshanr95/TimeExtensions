1dcaf9b5664fa1e2459abae6c5c920249ef8ca50
roshanr <thisisroshansmail@gmail.com>
2014-08-06 10:07:25 +0530

    Initial Commit

---
+++ TimeExtensions/TimeExtensions.h
@@ -0,0 +1,19 @@
+//
+//  TimeExtensions.h
+//  TimeExtensions
+//
+//  Created by Roshan Raghupathy on 06/08/14.
+//  Copyright (c) 2014 Roshan Raghupathy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TimeExtensions.
+FOUNDATION_EXPORT double TimeExtensionsVersionNumber;
+
+//! Project version string for TimeExtensions.
+FOUNDATION_EXPORT const unsigned char TimeExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeExtensions/PublicHeader.h>
+
+
